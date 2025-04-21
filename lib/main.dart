2ec2037f002b97d765f793ac5c7822ee555031a5
Aakash Kumar <gzatrop@gmail.com>
2025-04-22 00:30:17 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'auth_service.dart';
 import 'home_screen.dart';
 import 'login_screen.dart';
